da6db4050e61b10980dc4d4d3fb0e58ebf7c4cb8
desaikun1996 <desai.kun@northeastern.edu>
2022-06-06 16:51:50 -0400

    Update DataCleaning.sql

--- MySQL Files/DataCleaning.sql
+++ MySQL Files/DataCleaning.sql
@@ -9,13 +9,23 @@ select * from sales.markets;
 delete from sales.markets where markets_name in ('New York','Paris');
 
 
-/*
-Cleaning the Transactions table
 
-*/
+#Cleaning the Transactions table
+
 select * from sales.transactions order by sales_amount;
 select count(*) from sales.transactions where sales_amount<1;
-#There are 1611 records with sales_amount less than 0. We can go ahead with deleting these records as they may be outliers.
+# There are 1611 records with sales_amount less than 0. We can go ahead with deleting these records as they may be outliers.
 delete from sales.transactions where sales_amount<1;
+select count(*) from sales.transactions;	#148670
 
+# There are two records with currency as USD. 
+# To handle them, we will create a new column and insert all normalised amounts in it.
+select * from sales.transactions where currency='USD';
 
+alter table sales.transactions
+add column Normalised_Sales_Amount double after currency;
+
+update sales.transactions 
+set Normalised_Sales_Amount=case when currency='USD' then (sales_amount*77.89) else sales_amount end;
+
+select * from sales.transactions;

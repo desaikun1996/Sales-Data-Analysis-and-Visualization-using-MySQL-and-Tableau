54902ddb3e51d2422374ac1c1f84dcfe1389a8d5
desaikun1996 <desai.kun@northeastern.edu>
2022-06-06 11:48:57 -0400

    Update DataAnalysis1.sql

--- MySQL Files/DataAnalysis1.sql
+++ MySQL Files/DataAnalysis1.sql
@@ -12,11 +12,13 @@ select distinct zone from sales.markets;
 #Zone :- 1. South, 2. Central, 3. North, 4. Blank (New York, Paris)
 
 select distinct product_type from sales.products;
-#Product Types :- 1. Own Brand, 2. Distribution
+#Product Types :- 1. Own Brand, 2. Distribution (Both values with enter character - might need to clean)
 
 
 select * from sales.transactions;
 select count(*) from sales.transactions;	#150281
+select distinct currency from sales.transactions;
+#Currency :- INR, USD, INR(with enter character - Might need to clean)
 
 
 
